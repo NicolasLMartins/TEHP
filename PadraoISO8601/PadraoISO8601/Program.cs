c89d098564e323f895376e46c14fde0f3e07f2ec
NicolasLMartins <116754178+NicolasLMartins@users.noreply.github.com>
2023-03-30 16:49:44 -0300

    Pasta: Imagens

---
+++ PadraoISO8601/PadraoISO8601/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PadraoISO8601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local datetime
+
+            Console.WriteLine($"d1: {d1}");
+            Console.WriteLine($"d1: {d1.Kind}");
+            Console.WriteLine($"d1: {d1.ToLocalTime()}");
+            Console.WriteLine($"d1: {d1.ToUniversalTime()}");
+            Console.WriteLine();
+            Console.WriteLine($"d2: {d2}");
+            Console.WriteLine($"d2: {d2.Kind}");
+            Console.WriteLine($"d2: {d2.ToLocalTime()}");
+            Console.WriteLine($"d2: {d2.ToUniversalTime()}");
+            Console.WriteLine();
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado!
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+        }
+    }
+}
