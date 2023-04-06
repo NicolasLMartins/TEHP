6e5bfa87e8fa6aa9491ad01ff3033825108d52b3
web2henrique047 <web2henrique047@gmail.com>
2023-04-06 17:02:59 -0300

    Contribuintes (incompleto)

---
+++ Contribuintes/Contribuintes/Entities/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace Contribuintes.Entities
+{
+    abstract class Pessoa
+    {
+
+        public string Nome { get; set; }
+        public double RendaAnual { get; set; }
+
+        protected Pessoa(string nome, double rendaAnual)
+        {
+            Nome = nome;
+            RendaAnual = rendaAnual;
+        }
+
+
+        public abstract double Imposto();
+    }
+}
---
+++ Contribuintes/Contribuintes/Entities/PessoaJuridica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contribuintes.Entities
+{
+    class PessoaJuridica : Pessoa
+    {
+        private int NumeroFuncionarios { get; set; }
+
+        public PessoaJuridica(string nome, double rendaAnual, int numeroFuncionarios) : base(nome, rendaAnual)
+        {
+            NumeroFuncionarios = numeroFuncionarios;
+        }
+
+        public override double Imposto()
+        {
+            if (NumeroFuncionarios >= 25)
+            {
+                return (RendaAnual * 0.14);
+            }
+            else
+            { 
+                return RendaAnual * 0.16;
+            }
+        }
+    }
+}
